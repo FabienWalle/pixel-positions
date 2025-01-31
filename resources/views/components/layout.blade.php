a11b2c5ebb3d8b8d267398a18ed492f24c5ba8ef
Fabien Walle <fabienwalle13@gmail.com>
2025-01-31 15:29:22 +0100

    card and tag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,15 +9,15 @@
     <title>Pixel Positions</title>
 </head>
 
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white">
+    <div class="px-10 w-full">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
                 <a href="">
-                    <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="pixel position logo">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="pixel position logo">
                 </a>
             </div>
-            <div>
+            <div class="space-x-6 font-bold">
                 <a href="">Jobs</a>
                 <a href="">Careers</a>
                 <a href="">Salaries</a>
@@ -27,7 +27,7 @@
                 <a href="">Post a Job</a>
             </div>
         </nav>
-        <main>
+        <main class="mt-10 max-w-[986px] mx-auto">
             {{ $slot }}
         </main>
     </div>
