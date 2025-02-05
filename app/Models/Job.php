9cf1ff1ceb4cc25e19560ee8550adf37decf703f
Fabien Walle <fabienwalle13@gmail.com>
2025-02-05 10:15:21 +0100

    setting up tests

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,21 @@
 
 namespace App\Models;
 
+use Database\Factories\JobFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @method tag(string $string)
+ */
 class Job extends Model
 {
-    /** @use HasFactory<\Database\Factories\JobFactory> */
+    /** @use HasFactory<JobFactory> */
     use HasFactory;
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
